6342c6c93c50560031554073988aa1e246bd544a
Dale DuRose <dale.durose@gmail.com>
2022-04-07 12:41:10 +1200

    added live-server support

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const concat = require("gulp-concat");
 const uglify = require('gulp-uglify');
 const clean = require('gulp-clean');
 const sass = require('gulp-sass')(require('sass'));
+const liveserver = require('live-server')
 
 const paths = {
     javascript:'src/*.js',
@@ -12,6 +13,21 @@ const paths = {
     assets:'assets/*.*',
     dest:'dist'
 }
+
+const params = {
+    port: 8181, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: "dist", // Set root directory that's being served. Defaults to cwd.
+    open: true, // When false, it won't load your browser by default.
+    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+};
+
+function server(){
+    return liveserver.start(params);
+}
+
 
 function cleanDist(){
     return gulp.src(paths.dest+"/*", {read: false})
@@ -42,6 +58,8 @@ function watch(){
     gulp.watch([paths.javascript, paths.assets], gulp.series(css, javascript, assets))
 }
 
+exports.serve = server
 exports.watch = watch
+exports.clean = cleanDist
 exports.build = gulp.series(cleanDist, css, javascript, assets)
 exports.default = exports.build
